e128468aa8db6a0632aa16387c35fdba9697566d
EdwardLim1994 <edwardlimkoksiong1994@gmail.com>
2024-01-28 18:33:16 +0800

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
 import { Elysia } from "elysia";
+import { ollama, generateText } from "modelfusion";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().get("/", hello).listen(3000);
+
+async function hello() {
+	const text = await generateText({
+		model: ollama
+			.ChatTextGenerator({
+				model: "mistral",
+			})
+			.withTextPrompt(),
+		prompt: "What is the best programming language? \n\n",
+	});
+
+	return text;
+}
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
