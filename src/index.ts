3f1abe15ba812dcd7fc7e80e43c17046852f04c8
EdwardLim1994 <edwardlimkoksiong1994@gmail.com>
2024-01-31 20:40:09 +0800

    Main entry file with routes & middlewares defined

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,107 @@
-import { Elysia } from "elysia";
-import { ollama, generateText } from "modelfusion";
+import { Elysia, t } from "elysia";
+import { swagger } from "@elysiajs/swagger";
+import jwt from "@elysiajs/jwt";
 
-const app = new Elysia().get("/", hello).listen(3000);
+import {
+	findAccount,
+	createAccount,
+	deleteAccount,
+	refreshToken,
+} from "./controllers/AccountController";
+import { login, logout } from "./controllers/AuthenticationController";
+import {
+	if_user_login,
+	if_user_token_valid,
+	if_user_exist,
+} from "./middlewares/user.middleware";
+import { generateChat, streamChat } from "./controllers/ChatController";
 
-async function hello() {
-	const text = await generateText({
-		model: ollama
-			.ChatTextGenerator({
-				model: "mistral",
+const app = new Elysia()
+	.use(
+		swagger({
+			path: "/v2/api/doc",
+		})
+	)
+	.use(
+		jwt({
+			name: "jwt",
+			secret: Bun.env.JWT_SECRET!,
+		})
+	)
+	.group("/user", (app) =>
+		app
+
+			.post("/create", createAccount, {
+				body: t.Object({
+					name: t.String(),
+					keyword: t.String(),
+					password: t.String(),
+				}),
 			})
-			.withTextPrompt(),
-		prompt: "What is the best programming language? \n\n",
-	});
-
-	return text;
-}
+			.guard(
+				{
+					beforeHandle: if_user_exist,
+				},
+				(app) =>
+					app
+						.post("/login", login, {
+							body: t.Object({
+								username: t.String(),
+								password: t.String(),
+							}),
+						})
+						.post("/logout", logout, {
+							body: t.Object({
+								id: t.String(),
+							}),
+						})
+			)
+			.guard(
+				{
+					beforeHandle: if_user_exist,
+				},
+				(app) =>
+					app
+						.get("/get", findAccount, {
+							query: t.Object({
+								username: t.Optional(t.String()),
+								id: t.Optional(t.String()),
+							}),
+						})
+						.delete("/delete", deleteAccount, {
+							beforeHandle: if_user_token_valid,
+							body: t.Object({
+								id: t.String(),
+							}),
+						})
+						.post("token/refresh", refreshToken, {
+							beforeHandle: if_user_token_valid,
+							body: t.Object({
+								id: t.String(),
+								token: t.String(),
+							}),
+						})
+			)
+	)
+	.guard(
+		{
+			beforeHandle: if_user_login,
+		},
+		(app) =>
+			app.group("/chat", (app) =>
+				app
+					.get("/", generateChat, {
+						query: t.Object({
+							prompt: t.String(),
+						}),
+					})
+					.ws("/stream", {
+						message: streamChat,
+					})
+			)
+	)
+	.get("/", () => "Hello world")
+	.listen(3000);
 
 console.log(
 	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
